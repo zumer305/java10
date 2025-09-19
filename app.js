4d56b79b06e571792094254c829bd11e0dd6ff07
zumer305 <zumerniaz305@gmail.com>
2025-09-19 14:43:20 +0500

    update

--- app.js
+++ app.js
@@ -16,16 +16,43 @@
 
 let btns=document.querySelectorAll("button");
 for(btn of btns){
-    // 1
-    btn.onclick=sayhello;
-    // 2
-    btn.onmouseenter=function(){
-        console.log("You on mouse enter");
-    }
+    // // 1
+    // btn.onclick=sayhello;
+    // // 2
+    // btn.onmouseenter=function(){
+    //     console.log("You on mouse enter");
+    // }
+    btn.addEventListener("click",sayhello);
+      btn.addEventListener("click",sayname);
 }
  function sayhello(){
     alert("hi");
 }
+ function sayname(){
+    alert("zumer");
+}
+
+
+
+
+// Event listener (multiple work at a time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
