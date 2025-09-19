0d5bad08a12b6459f40368a641bf3f033c252c64
zumer305 <zumerniaz305@gmail.com>
2025-09-19 14:07:51 +0500

    update

---
+++ app.js
@@ -0,0 +1,32 @@
+// let btn=document.querySelector("button");
+// console.dir(btn);
+// btn.onclick=function(){
+//     console.log("Button was clicked");
+// }
+
+
+
+
+// function sayhello(){
+//     alert("hi");
+// }
+// btn.onclick=sayhello;
+
+
+
+let btns=document.querySelectorAll("button");
+for(btn of btns){
+    // 1
+    btn.onclick=sayhello;
+    // 2
+    btn.onmouseenter=function(){
+        console.log("You on mouse enter");
+    }
+}
+ function sayhello(){
+    alert("hi");
+}
+
+
+
+
