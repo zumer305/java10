1a032332b489550852d7b20aa9373fe12544a87d
zumer305 <zumerniaz305@gmail.com>
2025-09-19 17:05:32 +0500

    update

--- form.js
+++ form.js
@@ -1,5 +1,14 @@
 let form=document.querySelector("form");
 form.addEventListener("submit",function(event){
     event.preventDefault();
-    alert("form was submitted");
+    
+
+    let user=document.querySelector("#user");
+     let pass=document.querySelector("#pass");
+
+     console.log(user.value);
+     console.log(pass.value);
+
+     alert(`Hi ${user.value} your password is ${pass.value}`);
+
 });
